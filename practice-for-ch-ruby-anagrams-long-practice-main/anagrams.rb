de9b7a38ccbc4d34a78e8e8c5ef829d986368bc9
csmanel <csmanel@gmail.com>
2023-11-09 17:25:03 -0500

    initialize anagrams

---
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -0,0 +1,16 @@
+def first_anagram?(word1, word2)
+  arr = [] #store all possible anagrams of word1
+
+  
+
+
+end
+
+# def anagrams(str1, str2)
+#   hash1 = Hash.new(0)
+#   hash2 = Hash.new(0)
+
+#   str1.each_char { |hash1[char]+= 1}
+#   str2.each_char { |hash2[char]+= 1}
+#   hash1 == hash2
+# end
